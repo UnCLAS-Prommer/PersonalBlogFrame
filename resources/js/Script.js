8adf27ad3e27198fabc603652ef7fce7475038f0
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2023-03-18 00:06:14 +0800

    Fix Problems and add new functions

--- resources/js/Script.js
+++ resources/js/Script.js
@@ -33,63 +33,6 @@ function Command_screen(){
         }
     }
 }
-/*
-这一块是为了实现类似微信聊天那样的左右以及框功能的。构想是人名后加冒号，通过innerHTML加上<span>代码块，分离代码计算，生成效果。效果见聊天*/
-function Chat(myself,others,location,format,subjects){
-    var chat_num = location.getElementsByTagName("p").length;//判定个数
-    var text_list = location.getElementsByTagName("p");//获取组
-    var mlength = myself.length;//获取自己昵称长度
-    var olength = others.length;//他人
-    var text,text_front,text_last;//声明字符串拼接(text不是拼接用)
-    //函数
-    function Chat_Others(){
-        text_list[i].style.textAlign = "left";//设置位置
-        text_front = text.slice(0,olength);//截取前半
-        text_last = text.slice(olength);//截取后半
-        if(format == "QQ"){ //判断格式
-            text_list[i].innerHTML = text_front + '<span class = "chat_others_QQ">' + text_last + '</span>';
-        }
-        if(format == "WeChat"){
-            text_list[i].innerHTML = text_front + '<span class = "chat_others_WeChat">' + text_last + '</span>';//拼接计算
-        }
-    }
-    function Chat_Myself(){
-        text_list[i].style.textAlign = "right";
-        text_front = text.slice(0,-mlength);//从后向前截取(最终效果要求)
-        text_last = text.slice(-mlength);
-        if(subjects == "others"){//判定转发
-            if(format == "QQ"){
-                text_list[i].innerHTML = '<span class = "chat_myself_QQ_2">' + text_front + '</span>' + text_last;//转换格式拼接
-            }
-            if(format == "WeChat"){
-                text_list[i].innerHTML = '<span class = "chat_myself_WeChat_2">' + text_front + '</span>' + text_last;
-            }
-        }
-        else{//正常拼接
-            if(format == "QQ"){
-                text_list[i].innerHTML = '<span class = "chat_myself_QQ">' + text_front + '</span>' + text_last;
-            }
-            if(format == "WeChat"){
-                text_list[i].innerHTML = '<span class = "chat_myself_WeChat">' + text_front + '</span>' + text_last;
-            }
-        }
-    }
-    //主程序
-    for(var i = 0;i < chat_num;i++){
-        text = text_list[i].innerHTML;
-        if(text.search("<img") == -1){
-            if(text.slice(0,olength) == others && others != "empty"){ //判定只记录自言自语等情况
-                Chat_Others();
-            }
-            if(text.slice(-mlength) == myself && myself != "empty"){ //判定只记录别人独白等情况
-                Chat_Myself();
-            }
-        }
-        else{
-            text_list[i].getElementsByTagName("img")[0].style.verticalAlign = "top";
-        }
-    }
-}
 function Black_Curtains(){
     var cur_list = document.getElementsByTagName("hidding");//获取
     var cur_leng = cur_list.length;//长度，循环用
@@ -122,6 +65,7 @@ function Side_Bar(){
     Sidebar = !Sidebar;
 }
 
+//这一块是为了实现类似微信聊天那样的左右以及框功能的。构想是人名后加冒号，通过innerHTML加上<span>代码块，分离代码计算，生成效果。效果见聊天
 function Chat_new(myself,others,location,format,subjects){
     var chat_list = location.getElementsByTagName("chat"); // Get List
     var chat_num = chat_list.length;
